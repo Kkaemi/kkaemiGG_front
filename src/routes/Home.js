29baff021b7b0692e18fbc4d9c815856af881b39
SeongHyeon Ji <kitys2k3@naver.com>
2021-02-23 02:07:03 +0900

    기본 파일 삭제 헤더 생성

---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class Home extends Component {
+  render() {
+    return <Header></Header>;
+  }
+}
+
+export default Home;
